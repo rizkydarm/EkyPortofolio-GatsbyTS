273bc72c6a31ab8083080ff1c280cc851dbf2967
rizkyeky <mochamad.rizky.darmawan>
2025-02-12 09:27:19 +0700

    add dialog

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,18 +2,87 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+import { Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Grid2, List, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
 import FlutterIcon from '../svg/flutter';
+import AppleIcon from '../svg/apple';
+import AndroidIcon from '../svg/android';
+
+const emails = ['Add account', 'Profile', 'Logout'];
+
+interface SimpleDialogProps {
+  open: boolean;
+  handleClose: () => void;
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const { handleClose, open } = props;
+
+  const descriptionElementRef = React.useRef<HTMLElement>(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <Dialog open={open}
+      onClose={handleClose}
+      scroll="paper"
+      aria-labelledby="scroll-dialog-title"
+      aria-describedby="scroll-dialog-description"
+      maxWidth="xl" 
+      fullWidth
+      >
+      <DialogTitle id="scroll-dialog-title">Images</DialogTitle>
+      <DialogContent dividers>
+        <Grid2 container id="scroll-dialog-description"
+        rowSpacing={2} columnSpacing={2}
+          style={{ overflowY: 'auto' }}>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <Grid2 size={12/5} key={index}>
+              <Box
+                sx={{
+                  height: 400,
+                  backgroundColor: 'red',
+                  display: 'block',
+                  margin: 'auto'
+                }}
+              />
+            </Grid2>
+          ))}
+        </Grid2>
+      </DialogContent>
+      <DialogActions>
+        <Button style={{ fontWeight: 'bold', color: 'black' }} onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
 
 const BasicCard = ({ title, description, link }: {
   title: string;
   description: string;
   link: string;
 }) => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card variant='outlined' sx={{ height: 280 }}>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
-        <div> 
+        <div>
           <Typography variant="h5" component="div" fontWeight={'bold'}>
             {title}
           </Typography>
@@ -30,10 +99,9 @@ const BasicCard = ({ title, description, link }: {
             {description}
           </Typography>
           <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <FlutterIcon />
-            <Typography variant="body2" component="div" fontWeight={'bold'}>
-              Flutter
-            </Typography>
+            <FlutterIcon width={40} height={40} />
+            <AppleIcon width={40} height={40} />
+            <AndroidIcon width={40} height={40} />
           </div>
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
@@ -45,14 +113,21 @@ const BasicCard = ({ title, description, link }: {
           >
             Source Code
           </Button>
-          <Button variant='outlined'
+          <Button
+            variant='outlined'
             sx={{
               width: '48%',
-              color: 'primary', fontWeight: 'bold'
+              color: 'primary',
+              fontWeight: 'bold'
             }}
+            onClick={() => handleClickOpen()}
           >
             Images
           </Button>
+          <SimpleDialog
+            open={open}
+            handleClose={handleClose}
+          />
         </div>
       </CardContent>
     </Card>
--- src/svg/flutter.tsx
+++ src/svg/flutter.tsx
@@ -1,19 +1,24 @@
 import { SvgIcon, Box, SvgIconTypeMap } from '@mui/material';
 import React from 'react';
 
-const FlutterIcon = (props: any) => {
+const FlutterIcon = ({ width = 60, height = 60, ...props }: { width?: number; height?: number } & any) => {
   return (
-    <SvgIcon {...props}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="89.65" height="111.105" id="flutter">
-        <g fill-rule="evenodd">
-          <path fill="#02539a" d="m56.226 111.105-30.06-29.868 16.616-16.906 46.867 46.774Z"></path>
-          <g>
-            <path fill="rgba(69,209,253,0.85)" d="m25.795 81.26 29.8-29.8h33.5L42.736 97.818Z"></path>
-            <path fill="#45d1fd" d="m0 55.268 16.918 16.907L89.093 0H55.419Z"></path>
-          </g>
+    <Box
+      component="svg"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 89.65 111.105"
+      width={width}
+      height={height}
+      {...props}
+    >
+      <g fillRule="evenodd">
+        <path fill="#02539a" d="m56.226 111.105-30.06-29.868 16.616-16.906 46.867 46.774Z" />
+        <g>
+          <path fill="rgba(69,209,253,0.85)" d="m25.795 81.26 29.8-29.8h33.5L42.736 97.818Z" />
+          <path fill="#45d1fd" d="m0 55.268 16.918 16.907L89.093 0H55.419Z" />
         </g>
-      </svg>
-    </SvgIcon>
+      </g>
+    </Box>
   );
 };
 
